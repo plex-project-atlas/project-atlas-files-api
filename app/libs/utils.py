517bf2d46c19081c81c33f91ca1c4a1d89666886
mattia.pellin <mattia@pellin.it>
2022-06-27 21:41:39 +0200

    * Changed: extract all ops messages to enum

--- app/libs/utils.py
+++ app/libs/utils.py
@@ -3,9 +3,18 @@ import hashlib
 import logging
 import dateparser
 
+from   enum        import Enum
 from   pydantic    import BaseSettings, DirectoryPath, FilePath, ByteSize
 from   libs.models import MoveRequest, RenameRequest, OpsPrefightCheckResult
 
+
+class ActionMessages(str, Enum):
+    ERROR_SAME_NAME     = 'Name has not changed, no rename needed'
+    ERROR_SAME_PATH     = 'Path has not changed, no move required'
+    ERROR_NO_ACCESS     = 'Unable to access source file'
+    ERROR_SIZE_MISMATCH = 'Current file size does not match the provided one'
+    ERROR_HASH_MISMATCH = 'Current file hash does not match the provided one'
+    ERROR_DATE_MISMATCH = 'Current file modification date does not match the provided one'
 
 class Settings(BaseSettings):
     block_size:   ByteSize      = '128 MiB'
@@ -37,25 +46,25 @@ def do_ops_preflight_checks(file: RenameRequest | MoveRequest, block_size: int) 
 
     if src_file == dst_file:
         result.result = False
-        result.detail = f'Name has not changed, no rename needed' \
+        result.detail = ActionMessages.ERROR_SAME_NAME \
                         if isinstance(file, RenameRequest) else \
-                        f'Path has not changed, no move required'
+                        ActionMessages.ERROR_SAME_PATH
         logging.debug(f'[FilesAPI] - "{src_file}": {result.detail}')
     elif not ( file_hash := get_file_hash(src_file, block_size) ):
         result.result = False
-        result.detail = f'Unable to access source file'
+        result.detail = ActionMessages.ERROR_NO_ACCESS
         logging.warn(f'[FilesAPI] - "{src_file}": {result.detail}')
-    elif not ( ( file_size := os.path.getsize(src_file) ) == file.size ):
+    elif ( file_size := os.path.getsize(src_file) ) != file.size:
         result.result = False
-        result.detail = f'Current file size does not match the provided one'
+        result.detail = f'{ActionMessages.ERROR_SIZE_MISMATCH} (Delta: {abs(file_size - file.size)})'
         logging.warn(f'[FilesAPI] - "{src_file}": {result.detail}')
     elif file.hash and file_hash != file.hash:
         result.result = False
-        result.detail = f'Current file hash does not match the provided one'
+        result.detail = ActionMessages.ERROR_HASH_MISMATCH
         logging.warn(f'[FilesAPI] - "{src_file}": {result.detail}')
     elif file.mod_date and ( file_mod_date := dateparser.parse( str( os.path.getmtime(src_file) ) ) ) != file.mod_date:
         result.result = False
-        result.detail = f'Current file modification date does not match the provided one (Delta: {file_mod_date - file.mod_date})'
+        result.detail = f'{ActionMessages.ERROR_DATE_MISMATCH} (Delta: {file_mod_date - file.mod_date})'
         logging.warn(f'[FilesAPI] - "{src_file}": {result.detail}')
 
     return result
