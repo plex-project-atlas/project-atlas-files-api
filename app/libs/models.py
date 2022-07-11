f6f4a239d923adf1cd1e5b38a6a644f2289ba02d
mattia.pellin <mattia@pellin.it>
2022-07-11 12:31:25 +0200

    * Changed: specify NoneType where a model field could be None

--- app/libs/models.py
+++ app/libs/models.py
@@ -13,15 +13,15 @@ class TokenResponse(BaseModel):
 
 class OpsPrefightCheckResult(BaseModel):
     result: bool
-    detail: str = None
+    detail: str | None = None
 
 class File(BaseModel):
     name:      str
     path:      Path
-    hash:      str      = None
-    size:      ByteSize = None
-    mod_date:  datetime = None
-    mime_type: str      = None
+    hash:      str      | None = None
+    size:      ByteSize | None = None
+    mod_date:  datetime | None = None
+    mime_type: str      | None = None
 
 class RenameRequest(File):
     new_name: str
@@ -31,11 +31,11 @@ class MoveRequest(File):
 
 class RenameResponse(RenameRequest):
     renamed: bool
-    detail:  str = None
+    detail:  str | None = None
 
 class MoveResponse(MoveRequest):
     moved:  bool
-    detail: str = None
+    detail: str  | None = None
 
 class FileList(BaseModel):
     files: List[File]
